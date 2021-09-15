0853ea5088be45c5fce6578ad093bc09d06ab708
Pavel <sosnovskiy.pavel@inbox.ru>
2021-09-15 23:31:45 +0300

    fix: finish vigenere-chiper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,12 +22,87 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(straight = true) {
+    this.straight = straight;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+
+    if (message === undefined || key === undefined) {
+      throw new Error(`Incorrect arguments!`);
+    }
+
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+    let x = Math.ceil(message.length / key.length);
+    message = message.toUpperCase().split('');
+    key = key.repeat(x).toUpperCase().split('');
+    
+
+    let chiper = [],
+      posNum = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let count = -1;
+        let indexSum = alphabet.indexOf(message[i]) + alphabet.indexOf(key[posNum]);
+        let a = alphabet.length - 1 - indexSum;
+        
+        if (a < 0) {
+          count = Math.abs(a) - 1;
+        } else {
+          count = indexSum;
+        }
+        chiper.push(alphabet[count]);
+        posNum++;
+      } else {
+        chiper.push(message[i])
+      }
+    }
+
+    if (!this.straight) {
+      return chiper.reverse().join('');
+    } else {
+      return chiper.join('');
+    }
+
+  }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error(`Incorrect arguments!`);
+    }
+    
+    let x = Math.round(message.length / key.length);
+    message = message.toUpperCase().split('');
+    key = key.repeat(x).toUpperCase().split('');
+
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+    let chiper = [],
+      posNum = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let count = 1;
+        
+        if (1 + (alphabet.indexOf(message[i]) > alphabet.indexOf(key[posNum]))) {
+          count = alphabet.indexOf(message[i]) - alphabet.indexOf(key[posNum]);
+        } else {
+          count = alphabet.indexOf(key[posNum]) + alphabet.indexOf(message[i]);
+        }
+
+        if (count < 0) {
+          count = alphabet.length + count;
+        }
+
+        chiper.push(alphabet[count]);
+
+        posNum++;
+      } else {
+        chiper.push(message[i])
+      }
+    }
+    if (!this.straight) {
+      return chiper.reverse().join('');
+    } else {
+      return chiper.join('');
+    }
   }
 }
