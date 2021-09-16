034664e14bd694d8d2195df3a6b249f4e64776c8
Pavel <sosnovskiy.pavel@inbox.ru>
2021-09-17 00:39:11 +0300

    fix: st-mine-sweeper.js finish

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,7 +25,39 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper( matrix ) {
+  let mineMap = matrix.map(item => item.map(i => 0))
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i > 0) {
+        if (matrix[i - 1][j]) {
+          mineMap[i][j] += 1;
+        }  
+        if (matrix[i - 1][j - 1]) {
+          mineMap[i][j] += 1;
+        } 
+        if (matrix[i - 1][j + 1]) {
+          mineMap[i][j] += 1;
+        }
+      } 
+      if (i < matrix.length - 1) {
+        if (matrix[i + 1][j]) {
+          mineMap[i][j] += 1;
+        } 
+        if (matrix[i + 1][j - 1]) {
+          mineMap[i][j] += 1;
+        } 
+        if (matrix[i + 1][j + 1]) {
+          mineMap[i][j] += 1;
+        }
+      } 
+      if (matrix[i][j - 1]) {
+        mineMap[i][j] += 1;
+      } 
+      if (matrix[i][j + 1]) {
+        mineMap[i][j] += 1;
+      }
+    }
+  }
+  return mineMap
 }
